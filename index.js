e64356af666308af3cdba96e2fe91bd4e6a3de92
brendef <brendan.defaria@gmail.com>
2017-12-24 11:51:24 +0200

    saving

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const numbersArr = document.getElementsByClassName('numbers');
 const functions = document.getElementsByClassName('functions-right');
+let selectedFunction;
 let currentInput = [];
 let fullnumber;
 let total = 0;
 let fn = 0;
+let var1 = null;
+let var2 = null;
 let num1 = 0;
 let num2 = 0;
-
-function plus(number) {
-  fn = parseInt(number, 10);
-  total += fn;
-  fullnumber = 0;
-  currentInput.length = 0;
-}
-
-function minus(number) {
-  fn = parseInt(number, 10);
-  total -= fn;
-  fullnumber = 0;
-  currentInput.length = 0;
-}
 
 for(let i = 0; i < numbersArr.length; i++) {
   numbersArr[i].addEventListener("click", function() {
@@ -27,24 +16,109 @@ for(let i = 0; i < numbersArr.length; i++) {
       fullnumber = currentInput.join("");
       tfn = fullnumber.replace(/\s/g,'');
       document.getElementById("nums").innerHTML = tfn;
+
+      if (var1 != null) {
+        var2 = tfn;
+      }
+
+      if (var2 != null) {
+        if (selectedFunction == "plus") {
+          num1 = parseInt(var1, 10);
+          num2 = parseInt(var2, 10);
+          total += num1 + num2;
+          // var1 = null;
+          // var2 = null;
+          // fullnumber = 0;
+          // currentInput.length = 0;
+        } else if (selectedFunction == "minus") {
+          num1 = parseInt(var1, 10);
+          num2 = parseInt(var2, 10);
+          total -= num1 - num2;
+          // var1 = null;
+          // var2 = null;
+          // fullnumber = 0;
+          // currentInput.length = 0;
+        }
+      }
+
   });
 }
 
 for(let i = 0; i < functions.length; i++) {
   functions[i].addEventListener("click", function() {
-      if(functions[i].id == "plus"){
-        plus(tfn);
-        document.getElementById("nums").innerHTML = total;
+    if(functions[i].id == "plus"){
+      if (var2 != null) {
+        fullnumber = 0;
+        currentInput.length = 0;
+        var1 = null;
+        var2 = null;
       }
-      if(functions[i].id == "minus"){
-        minus(tfn);
-        document.getElementById("nums").innerHTML = total;
+
+      if (var1 == null) {
+        var1 = tfn;
+        fullnumber = 0;
+        currentInput.length = 0;
+      } else {
+        document.getElementById("nums").innerHTML = "error";
       }
-      if(functions[i].id == "multiply") {
-      }
-      if (functions[i].id == "devide"){
-      }
+      selectedFunction = "plus";
+    }
+    if(functions[i].id == "minus"){
+      selectedFunction = "minus";
+    }
+    if(functions[i].id == "multiply") {
+      selectedFunction = "multiply";
+    }
+    if (functions[i].id == "devide"){
+      selectedFunction = "devide";
+    }
   });
 }
 
+if (var2 != null) {
+  document.getElementById("nums").innerHTML = "var2 is full";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  setInterval(function(){ document.getElementById("l").innerHTML = total; }, 100);
+ setInterval(function(){ document.getElementById("var1").innerHTML = var1; }, 100);
+ setInterval(function(){ document.getElementById("var2").innerHTML = var2; }, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+ // function plus(number) {
+ //   fn = parseInt(number, 10);
+ //   total += fn;
+ //   fullnumber = 0;
+ //   currentInput.length = 0;
+ // }
+ //
+ // function minus(number) {
+ //   fn = parseInt(number, 10);
+ //   total -= fn;
+ //   fullnumber = 0;
+ //   currentInput.length = 0;
+ // }
