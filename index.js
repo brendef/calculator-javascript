a1780511988e56ea92fabfb496a6f88efedb5e8b
brendef <brendan.defaria@gmail.com>
2018-01-02 09:33:59 +0200

    * add and minus not working 100# * most efficient and clean code yet

--- index.js
+++ index.js
@@ -1,124 +1,42 @@
-const numbersArr = document.getElementsByClassName('numbers');
-const functions = document.getElementsByClassName('functions-right');
-let selectedFunction;
-let currentInput = [];
-let fullnumber;
-let total = 0;
-let fn = 0;
-let var1 = null;
-let var2 = null;
-let num1 = 0;
-let num2 = 0;
+// All 'Const' Variables Created Here.
+const functions      = document.getElementsByClassName('functions-right');
+const numbersArr     = document.getElementsByClassName('numbers');
+
+// All 'Let' Variables Created Here.
+let fn      = 0;
+let tfn     = "";
+let total   = 0;
+let answers = [];
+let selectedFunction = null;
 
 for(let i = 0; i < numbersArr.length; i++) {
   numbersArr[i].addEventListener("click", function() {
-      currentInput.push(numbersArr[i].innerText);
-      fullnumber = currentInput.join("");
-      tfn = fullnumber.replace(/\s/g,'');
+      tfn = tfn.concat(numbersArr[i].innerText).replace(/\s/g,'');
       document.getElementById("nums").innerHTML = tfn;
 
-      if (var1 != null) {
-        var2 = tfn;
+      if (selectedFunction == "plus") {
+        fn = parseInt(tfn, 10);
       }
 
-      if (var2 != null) {
-        if (selectedFunction == "plus") {
-          num1 = parseInt(var1, 10);
-          num2 = parseInt(var2, 10);
-          total += num1 + num2;
-          // var1 = null;
-          // var2 = null;
-          // fullnumber = 0;
-          // currentInput.length = 0;
-        } else if (selectedFunction == "minus") {
-          num1 = parseInt(var1, 10);
-          num2 = parseInt(var2, 10);
-          total -= num1 - num2;
-          // var1 = null;
-          // var2 = null;
-          // fullnumber = 0;
-          // currentInput.length = 0;
-        }
+      if (selectedFunction == "minus") {
+        fn = parseInt(tfn, 10) * -1;
       }
+      answers.push(fn);
 
+      answers[answers.length]
+
+      // for (let i = 0; i < answers.length; i++) {
+      //   total += answers[answers.length] - answers[i];
+      // }
   });
 }
 
 for(let i = 0; i < functions.length; i++) {
   functions[i].addEventListener("click", function() {
-    if(functions[i].id == "plus"){
-      if (var2 != null) {
-        fullnumber = 0;
-        currentInput.length = 0;
-        var1 = null;
-        var2 = null;
-      }
-
-      if (var1 == null) {
-        var1 = tfn;
-        fullnumber = 0;
-        currentInput.length = 0;
-      } else {
-        document.getElementById("nums").innerHTML = "error";
-      }
-      selectedFunction = "plus";
-    }
-    if(functions[i].id == "minus"){
-      selectedFunction = "minus";
-    }
-    if(functions[i].id == "multiply") {
-      selectedFunction = "multiply";
-    }
-    if (functions[i].id == "devide"){
-      selectedFunction = "devide";
-    }
+    tfn = "";
+    fn  = 0;
+    selectedFunction = functions[i].id;
   });
 }
 
-if (var2 != null) {
-  document.getElementById("nums").innerHTML = "var2 is full";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- setInterval(function(){ document.getElementById("l").innerHTML = total; }, 100);
- setInterval(function(){ document.getElementById("var1").innerHTML = var1; }, 100);
- setInterval(function(){ document.getElementById("var2").innerHTML = var2; }, 100);
-
-
-
-
-
-
-
-
-
-
-
-
- // function plus(number) {
- //   fn = parseInt(number, 10);
- //   total += fn;
- //   fullnumber = 0;
- //   currentInput.length = 0;
- // }
- //
- // function minus(number) {
- //   fn = parseInt(number, 10);
- //   total -= fn;
- //   fullnumber = 0;
- //   currentInput.length = 0;
- // }
+setInterval(function(){ document.getElementById("answer").innerHTML = total; }, 1);
