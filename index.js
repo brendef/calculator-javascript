7f57dbf47f330111f85b931eb27cb0e9a6ff4950
brendef <brendan.defaria@gmail.com>
2018-01-16 23:50:04 +0200

    - addition functionality almost 100% working like I want it to

--- index.js
+++ index.js
@@ -3,40 +3,30 @@ const functions      = document.getElementsByClassName('functions-right');
 const numbersArr     = document.getElementsByClassName('numbers');
 
 // All 'Let' Variables Created Here.
-let fn      = 0;
-let tfn     = "";
-let total   = 0;
-let answers = [];
+let n         = 0;
+let fn        = 0;
+let tfn       = "";
+let total     = 0;
+let answers   = [0];
+let tempTotal = 0;
 let selectedFunction = null;
 
 for(let i = 0; i < numbersArr.length; i++) {
   numbersArr[i].addEventListener("click", function() {
       tfn = tfn.concat(numbersArr[i].innerText).replace(/\s/g,'');
       document.getElementById("nums").innerHTML = tfn;
-
-      if (selectedFunction == "plus") {
-        fn = parseInt(tfn, 10);
-      }
-
-      if (selectedFunction == "minus") {
-        fn = parseInt(tfn, 10) * -1;
-      }
-      answers.push(fn);
-
-      answers[answers.length]
-
-      // for (let i = 0; i < answers.length; i++) {
-      //   total += answers[answers.length] - answers[i];
-      // }
+      fn = parseInt(tfn, 10);
+      tempTotal = total + fn;
   });
 }
 
 for(let i = 0; i < functions.length; i++) {
   functions[i].addEventListener("click", function() {
+    total += fn;
     tfn = "";
     fn  = 0;
     selectedFunction = functions[i].id;
   });
 }
 
-setInterval(function(){ document.getElementById("answer").innerHTML = total; }, 1);
+setInterval(function(){ document.getElementById("answer").innerHTML = tempTotal; }, 1);
