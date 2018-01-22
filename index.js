2137a203f7da298f748893f6b1b8d91f64957cc5
brendef <brendan.defaria@gmail.com>
2018-01-22 14:00:37 +0200

    - All Functions Working - Able To Clear - Equals Sign Working - Show Current Sign

--- index.js
+++ index.js
@@ -7,37 +7,50 @@ let fn        = 0;
 let tfn       = "";
 let total     = 0;
 let tempTotal = 0;
+let selectedSign = "=";
 let selectedFunction = null;
 
+// Act On button Clicks And Work Out Temporary Total
 for(let i = 0; i < numbersArr.length; i++) {
   numbersArr[i].addEventListener("click", function() {
       tfn = tfn.concat(numbersArr[i].innerText).replace(/\s/g,'');
       document.getElementById("nums").innerHTML = tfn;
-      fn = parseInt(tfn, 10);
-
+      fn = parseFloat(tfn, 10);
       if (selectedFunction == "minus") {
         fn = fn * -1;
       }
-
       if (selectedFunction == "multiply") {
         fn = (total * fn) - total;
       }
-
       if (selectedFunction == "devide") {
         fn = (total / fn) - total;
       }
-
       tempTotal = total + fn;
   });
 }
 
+// Act On All Function Clicks and Work Out Total
 for(let i = 0; i < functions.length; i++) {
   functions[i].addEventListener("click", function() {
-    total += fn;
-    tfn = "";
-    fn  = 0;
     selectedFunction = functions[i].id;
+    selectedSign = functions[i].innerText;
+    document.getElementById("nums").innerHTML = "";
+    if (selectedFunction == "clear") {
+      document.getElementById("nums").innerHTML = "";
+      tfn = "";
+      fn  = 0;
+      tempTotal = total;
+    } else {
+      total += fn;
+      tfn = "";
+      fn  = 0;
+    }
+    if (selectedFunction == "equals") {
+      document.getElementById("operator").innerHTML =  selectedSign;
+    }
   });
 }
 
-setInterval(function(){ document.getElementById("answer").innerHTML = tempTotal; }, 1);
+// Display Results On Screen
+setInterval(function(){ document.getElementById("answer").innerHTML = tempTotal ; }, 1);
+setInterval(function(){ document.getElementById("operator").innerHTML =  selectedSign ; }, 1);
